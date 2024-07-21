aefd8ea2120ed94e67a6fa8c4212cd9dc2cc130e
iwauchi-ori <shota.iwauchi@orithankyou.co.jp>
2024-07-21 22:36:57 +0900

    controller

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,30 +4,29 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.http._
+import play.api.data._
+import play.api.data.Forms._
 import org.apache.pekko.util._
-import java.util._
 
-
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(cc: MessagesControllerComponents)
+     extends MessagesAbstractController(cc) {
+  import MyForm._
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index(p:Option[Int]) = Action {
-    val arr:scala.collection.immutable.List[scala.collection.immutable.List[String]] = scala.collection.immutable.List(
-      scala.collection.immutable.List("taro", "taro@yamada","999"),
-      scala.collection.immutable.List("hanako", "hanako@yamada","998"),
-      scala.collection.immutable.List("jiro", "jiro@yamada","997"),
-    )
-    Ok(views.html.index("これはコントローラーで使用したメッセージ。", arr, scala.collection.immutable.List("Name","Mail","Tel")))
+  def index() = Action {implicit request =>
+    Ok(views.html.index(
+      "これはコントローラーで用意したメッセージ。",
+      myform
+      ))
+  }
+
+  def form() = Action { implicit request =>
+    val form = myform.bindFromRequest
+    val data = form.get
+    Ok(views.html.index(
+        "name: " + data.name + ", pass: " + data.pass +
+          ", radio:" + data.radio,
+        form
+    ))
   }
 }
